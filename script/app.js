29741d025ccc78cb4abdf4f3d99804df4422c507
arililhamghifari <arililhamghifari@gmail.com>
2022-09-21 22:54:55 +0700

    fix populer dishes

--- script/app.js
+++ script/app.js
@@ -1,33 +1,45 @@
 // swiper slider popular dishes
 var swiper = new Swiper(".slide-content", {
-    slidesPerView: 4,
-    spaceBetween: 20,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".button-next",
-      prevEl: ".button-prev",
-    },
+  slidesPerView: 4,
+  spaceBetween: 20,
+  // observer: true,
+  // observeParents: true,
+  loop: true,
+  centerSlide: true,
+  centeredSlides: true,
+  // slideToClickedSlide: true,
+  // watchSlidesVisibility: true,
+  // watchSlidesProgress: true,
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
 
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-        },
-        950: {
-            slidesPerView: 3,
-        },
-        950: {
-          slidesPerView: 4,
-        },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-  });
+    520: {
+      slidesPerView: 2,
+    },
+    520: {
+      slidesPerView: 3,
+    },
+    520: {
+      slidesPerView: 4,
+    },
+    1250: {
+      slidesPerView: 5,
+    },
+    1250: {
+      slidesPerView: 6,
+    },
+  },
+});
